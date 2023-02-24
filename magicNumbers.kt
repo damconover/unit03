83f0130e97be017fbeb99adc06c34502ccbd2c6c
damconover <123193877+damconover@users.noreply.github.com>
2023-02-24 01:36:17 -0600

    Create magicNumbers.kt

---
+++ magicNumbers.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    print("Enter a month in numeric form, a two digit day, and two digit year: ")
+    val month = 1..12
+    val day = 1..31
+    val year = int
+  when (year == day * month) 
+    is String -> println("The date is magic.")
+    else -> println("The date is not magic.")
+}
