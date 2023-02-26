1af57868206d1392d18ce94828f3911c1ce7d35c
damconover <123193877+damconover@users.noreply.github.com>
2023-02-26 14:43:44 -0600

    Create nameSort.kt

---
+++ nameSort.kt
@@ -0,0 +1,9 @@
+fun main() {
+    print("Charlie, Leslie, Andy:")
+    val inputStrings = ("Andy, Charlie, Leslie")
+    val sortedStrings = inputStrings    
+    
+    println("The sorted names are: ")
+    sortedStrings.forEach { println(it) }
+    
+}
