a9cbc7123e967f2d1f49dcfc6a5ee01f1506a68e
damconover <123193877+damconover@users.noreply.github.com>
2023-03-30 13:22:02 -0500

    Update romans.kt

--- romans.kt
+++ romans.kt
@@ -1,7 +1,23 @@
-fun main(args : Array<String>) {
-    println("Enter a number between 1 and 10.")
-}
-var number 
-if (number >1 , <10) {
-  println("Error")
+fun main() {
+    println("enter a number within the range of 1 through 10")
+    val number = readln().toInt()
+
+    if(number > 10 || number < 0) {
+        println("error, the number is not valid");
+        return;
+    }
+
+    if (number == 1) println("I")
+    else if( number == 2) println("II")
+    else if( number == 3) println("III")
+    else if( number == 4) println("IV")
+    else if( number == 5) println("V")
+    else if( number == 6) println("VI")
+    else if( number == 7) println("VII")
+    else if( number == 8) println("VIII")
+    else if( number == 9) println("IX")
+    else if( number == 10) println("X")
+
+
+
 }
