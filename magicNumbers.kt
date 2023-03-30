fb5cb9fe1445b1ab4eae46d23bbe17a58f9e492c
damconover <123193877+damconover@users.noreply.github.com>
2023-03-30 13:23:34 -0500

    Update magicNumbers.kt

--- magicNumbers.kt
+++ magicNumbers.kt
@@ -1,9 +1,35 @@
-fun main(args: Array<String>) {
-    print("Enter a month in numeric form, a two digit day, and two digit year: ")
-    val month = 1..12
-    val day = 1..31
-    val year = int
-  when (year == day * month) 
-    is String -> println("The date is magic.")
-    else -> println("The date is not magic.")
+fun main() {
+    println("Enter month")
+
+    val month = readln().toInt()
+
+    if (month > 12 || month < 1) {
+        println("error, number is not logic");
+        return;
+    }
+
+    println("Enter a day")
+    val day = readln().toInt()
+
+    if (day > 31 || day < 1) {
+        println("error, number is not logic");
+        return;
+    }
+
+    println("Enter a year")
+    val year = readln().toInt()
+    if (year < 1) {
+        println("error, number is not logic")
+    }
+
+    val multiplication = month * day
+    if( multiplication ==year) {
+        println("date is magic")
+    }
+    else
+            println("date is not magic")
+
+
 }
+
+main()
