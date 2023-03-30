322bdf65158f80937019fed2776d65fe153011b1
damconover <123193877+damconover@users.noreply.github.com>
2023-03-30 13:28:04 -0500

    Update nameSort.kt

--- nameSort.kt
+++ nameSort.kt
@@ -1,9 +1,11 @@
 fun main() {
-    print("Charlie, Leslie, Andy:")
-    val inputStrings = ("Andy, Charlie, Leslie")
-    val sortedStrings = inputStrings    
-    
-    println("The sorted names are: ")
-    sortedStrings.forEach { println(it) }
-    
+    print("Please enter the first name:")
+    val firstName = readLine()!!
+    print("Please enter the second name:")
+    val secondName = readLine()!!
+    print("Please enter the third name:")
+    val thirdName = readLine()!!
+    val namesList = listOf(firstName, secondName, thirdName)
+    println("The names sorted in ascending order are:")
+    namesList.sorted().forEach { println(it) }
 }
